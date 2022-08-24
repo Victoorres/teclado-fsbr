f79611eb07cbd8fe7027a7822641d3998b1966ec
Victor Torres <victoorres@icloud.com>
2022-08-24 12:53:02 -0300

    HEROKU DEPLOYMENT

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use(function(req, res, next) {
 app.use(express.static("www"));
 
 app.set("port", process.env.PORT || 5000);
-app.listen(app.get(~port), function () {
+app.listen(app.get('port'), function () {
     console.log("Express server listening on port" + app.get("port"));
 })
